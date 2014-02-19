a80cb446dadf107f96aede4b34ea4cc4c7af6459
novellus <novellus.cato@gmail.com>
2014-02-19 15:56:30 -0600

    Update Point-Generator.py
    Fixed bugs

--- Point-Generator.py
+++ Point-Generator.py
@@ -1,27 +1,35 @@
 class LorenzGenerator:
-	x=0
-	y=0
-	z=0
+	x=1
+	y=1
+	z=1
 	sigma=10
 	rho=28
 	beta=8.0/3
-	timeStep=.01
+	dt=.01
 
 	def __init__(self,sigma=10,rho=28,beta=8.0/3):
 		self.sigma=sigma
 		self.rho=rho
 		self.beta=beta
 
-	def start(self,x0=0,y0=0,z0=0,timeStep=0.01):
+	def start(self,x0=1,y0=1,z0=1,timeStep=0.01):
 		self.x=x0
 		self.y=y0
 		self.z=z0
-		self.timeStep=timeStep
+		self.dt=timeStep
 
 	def setTimestep(self,timeStep):
-		self.timeStep=timeStep
+		self.dt=timeStep
 
 	def getNextPoint(self):
+		dx=(self.sigma*(self.y-self.x))*self.dt
+		dy=(self.x*(self.rho-self.z)-self.y)*self.dt
+		dz=(self.x*self.y-self.beta*self.z)*self.dt
+		self.x+=dx
+		self.y+=dy
+		self.z+=dz
+		return (self.x,self.y,self.z)
+
 		dx=(sigma*(y-x))*dt
 		dy=(x*(rho-z)-y)*dt
 		dz=(x*y-beta*z)*dt
